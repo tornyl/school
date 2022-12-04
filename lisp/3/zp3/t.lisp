ff5c1f0739a4ddc63a996808f4ef559aea3c41e7
Tornyl <luki.matejka@email.cz>
2022-12-04 13:44:20 +0100

    1

--- lisp/3/zp3/t.lisp
+++ lisp/3/zp3/t.lisp
@@ -1,11 +1,14 @@
 (defun make-circle (color x y)
 	(move (set-radius (set-filledp (set-color (make-instance 'circle) color) t) 30) x y))
 
+(setf poly (move (make-arrow :green) 200 300))
+
+
 (setf ins-w (make-instance 'inspected-window))
 (setf ior-w (make-instance 'inspector-window))
 
 (setf p (make-instance 'picture))
-(set-items p (list (make-circle  :red 100 100) (make-circle  :blue 300 100)))
+(set-items p (list (make-circle  :red 100 100) (make-circle  :blue 300 100) poly))
 
 (set-shape ins-w p)
 (set-inspected-window ior-w ins-w)
